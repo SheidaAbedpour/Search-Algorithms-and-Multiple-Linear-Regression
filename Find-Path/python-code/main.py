62931f049da5fb0d7c62c9603bbbac0c5cc85ae4
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2023-11-04 23:32:41 +0330

    graph created

--- Find-Path/python-code/main.py
+++ Find-Path/python-code/main.py
@@ -4,4 +4,14 @@ import pandas as pd
 df = pd.read_csv('Flight_Data.csv')
 #print(df)
 
+# create graph of airports
+graph = {}
+for i, row in df.iterrows():
+    source = row['SourceAirport']
+    destination = row['DestinationAirport']
+    if source not in graph:
+        graph[source] = {}
+    if destination not in graph:
+        graph[destination] = {}
+    graph[source][destination] = i
 
