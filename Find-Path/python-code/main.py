44be09efac7bbc5640510735b442f03b7605fa19
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2023-11-06 00:18:51 +0330

    some changes in graph for better performance

--- Find-Path/python-code/main.py
+++ Find-Path/python-code/main.py
@@ -2,9 +2,38 @@ import numpy as np
 import pandas as pd
 import heapq
 import math
+from timeit import default_timer as timer
 
 df = pd.read_csv('Flight_Data.csv')
 #print(df)
+
+
+
+# create graph of airports
+graph = {}
+# stor location of each airport to compute heuristic faster
+location = {}
+
+for i, row in df.iterrows():
+    source = row['SourceAirport']
+    destination = row['DestinationAirport']
+    if source not in graph:
+        graph[source] = {}
+    if destination not in graph:
+        graph[destination] = {}
+    graph[source][destination] = row
+
+    if source not in location:
+        location[source] = {}
+    if destination not in location:
+        location[destination] = {}
+    location[source] = [row['SourceAirport_Latitude'],
+                        row['SourceAirport_Longitude'],
+                        row['SourceAirport_Altitude']]
+    location[destination] = [row['DestinationAirport_Latitude'],
+                             row['DestinationAirport_Longitude'],
+                             row['DestinationAirport_Altitude']]
+
 
 
 # compte weight of graph based on distance, price and flyTime
@@ -19,41 +48,21 @@ def compute_weight(distance, price, flyTime):
 
 
 
-# create graph of airports
-graph = {}
-for i, row in df.iterrows():
-    source = row['SourceAirport']
-    destination = row['DestinationAirport']
-    if source not in graph:
-        graph[source] = {}
-    if destination not in graph:
-        graph[destination] = {}
-    graph[source][destination] = compute_weight(row['Distance'], row['Price'], row['FlyTime'])
-
-
-
 # dijkstra algorithm
 def dijkstra(graph, source, destination):
 
-    # store distances from source to every node
     distances = {n: float('inf') for n in graph.keys()}
     distances[source] = 0
 
-    priority_queue = [(0, source)]
+    priority_queue = [(0, source)]  # (distance, node)
 
-    # store the path
     pervious = {n: None for n in graph.keys()}
 
     while priority_queue:
-
-        # get node with minimum distance
         cur_distance, cur_node = heapq.heappop(priority_queue)
-
-        if cur_node == destination:
-            return cur_distance, pervious
-
-        # update distance of neighbors
-        for neighbor, weight in graph[cur_node].items():
+        if cur_node == destination: return cur_distance, pervious
+        for neighbor, row in graph[cur_node].items():
+            weight = compute_weight(row['Distance'], row['Price'], row['FlyTime'])
             new_distance = cur_distance + weight
             if new_distance < distances[neighbor]:
                 distances[neighbor] = new_distance
@@ -62,7 +71,7 @@ def dijkstra(graph, source, destination):
 
 
 
-# get path from source to destination
+            # get path from source to destination
 def get_path(pervious, destination):
     path = []
     current = destination
@@ -76,87 +85,79 @@ def get_path(pervious, destination):
 airport_source = 'Imam Khomeini International Airport'
 airport_destination = 'Raleigh Durham International Airport'
 
+start_time = timer()
 min_distance, pervious = dijkstra(graph, airport_source, airport_destination)
+end_time = timer()
+
 path = get_path(pervious, airport_destination)
 
 print(min_distance)
 print(" > ".join(path))
+
+time = end_time - start_time
+print("time : ", time)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 # A* algorithm
 
-def heurestic(source, destination):
+def heurestic(row_source, row_destination):
+    d1_source = row_source['SourceAirport_Latitude']
+    d2_source = row_source['SourceAirport_Longitude']
+    d3_source = row_source['SourceAirport_Altitude']
 
-    # if there is no path from source to destination return
-    if df[(df['SourceAirport'] == source) & (df['DestinationAirport'] == destination)].empty:
-        return 0;
+    d1_destination = row_destination['DestinationAirport_Latitude']
+    d2_destination = row_destination['DestinationAirport_Longitude']
+    d3_destination = row_destination['DestinationAirport_Altitude']
 
-    # get row in df
-    row_source = df[df['SourceAirport'] == source].iloc[0]
-    row_destination = df[df['DestinationAirport'] == destination].iloc[0]
-
-    #get index of row
-    index_source = df[df.eq(row_source).all(axis=1)].index[0]
-    index_destination = df[df.eq(row_destination).all(axis=1)].index[0]
-
-    # get location of source
-    d1_source = df.loc[index_source, 'SourceAirport_Latitude']
-    d2_source = df.loc[index_source, 'SourceAirport_Longitude']
-    d3_source = df.loc[index_source, 'SourceAirport_Altitude']
-
-    # get location of destination
-    d1_destination = df.loc[index_destination, 'DestinationAirport_Latitude']
-    d2_destination = df.loc[index_destination, 'DestinationAirport_Longitude']
-    d3_destination = df.loc[index_destination, 'DestinationAirport_Altitude']
-
-    # compute distance
     d1 = d1_source - d1_destination
     d2 = d2_source - d2_destination
     d3 = d3_source - d3_destination
 
     distance = math.sqrt(d1 ** 2 + d2 ** 2 + d3 ** 2)
-    price = df['Price'][index_source]
-    flyTime = df['FlyTime'][index_source]
+    # price = row['Price']
+    # flyTime = row['FlyTime']
 
-    weight = compute_weight(distance, price, flyTime)
-    return weight
+    # weight = compute_weight(distance, price, flyTime)
+    return distance
 
 
 def a_star(graph, source, destination):
-
-    # define g(x) and f(x)
     g = {n: float('inf') for n in graph.keys()}
     f = {n: float('inf') for n in graph.keys()}
 
     priority_queue = [(0, source)]
     pervious = {n: None for n in graph.keys()}
 
+    row_source = df[df['SourceAirport'] == source].iloc[0]
+    row_destination = df[df['DestinationAirport'] == destination].iloc[0]
+
     g[source] = 0
-    f[source] = heurestic(source, destination)
+    f[source] = heurestic(row_source, row_destination)
 
     while priority_queue:
-
-        # get the node with min distance
         cur_distance, cur_node = heapq.heappop(priority_queue)
-
         if cur_node == destination:
             return cur_distance, pervious
-
-        # update distance of neighbors
-        for neighbor, weight in graph[cur_node].items():
+        for neighbor, row in graph[cur_node].items():
+            weight = row['Distance']
             new_distance = g[cur_node] + weight
             if new_distance < g[neighbor]:
                 g[neighbor] = new_distance
-                f[neighbor] = g[neighbor] + heurestic(neighbor, destination)
+                f[neighbor] = g[neighbor] + heurestic(row, row_destination)
                 pervious[neighbor] = cur_node
                 heapq.heappush(priority_queue, (f[neighbor], neighbor))
 
 
 # test A* algorithm
-#min_d, pervious = a_star(graph, airport_source, airport_destination)
-#path = get_path(pervious, airport_destination)
+start_time = timer()
+min_d, pervious = a_star(graph, airport_source, airport_destination)
+end_time = timer()
 
-#print(min_d)
-#print(" > ".join(path))
+path = get_path(pervious, airport_destination)
+
+print(min_d)
+print(" > ".join(path))
+time = end_time - start_time
+print("time : ", time)
