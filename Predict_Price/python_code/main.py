c2b640acf01274f725621c4cd9ea87defba1470f
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2023-11-12 00:03:33 +0330

    debug and print final result

--- Predict_Price/python_code/main.py
+++ Predict_Price/python_code/main.py
@@ -5,22 +5,31 @@ import copy
 import math
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import time
 
 
 df = pd.read_csv('Flight_Price_Dataset_Q2.csv')
 #print(df.head(10))
 
-dummies_columns = ['departure_time', 'stops', 'arrival_time', 'class']
+dummies_columns = ['class']
 for dummy in dummies_columns:
     dummies = pd.get_dummies(df[dummy], drop_first=True).astype(int)
     df = pd.concat([df, dummies], axis='columns')
     df = df.drop([dummy], axis='columns')
 
+time_mapping = {'Early_Morning': 1, 'Morning': 2, 'Afternoon': 3, 'Evening': 4, 'Night': 5, 'Late_Night': 6}
+df['departure_time'] = df['departure_time'].map(time_mapping)
+df['arrival_time'] = df['arrival_time'].map(time_mapping)
+
+stop_mapping = {'zero': 0, 'one': 1, 'two_or_more': 2}
+df['stops'] = df['stops'].map(stop_mapping)
+
 scaler = StandardScaler()
 df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
 
-x = df.drop('price', axis = 'columns').values
+df_x = df.drop('price', axis = 'columns')
+x = df_x.values
 y = df['price'].values
 
 x_train, x_test, y_train , y_test = train_test_split(x, y, test_size = 0.2,random_state = 42, shuffle = True)
@@ -64,8 +73,7 @@ def compute_gradient_descent(X, Y, w_in, b_in, alpha, num_iterations):
     w = copy.deepcopy(w_in)
 
     J_history = []
-    w_history = []
-    b_history = []
+    y_predict = []
 
     for i in range(num_iterations):
         dj_dw, dj_db = compute_gradient(X, w, b, Y)
@@ -81,9 +89,9 @@ def compute_gradient_descent(X, Y, w_in, b_in, alpha, num_iterations):
         r2 = r2_score(Y, f_k)
 
         # if i % math.ceil(num_iterations / 1000) == 0:
-        print("iteration: ", i + 1, "   cost: ", cost, "r2: ", r2)
+        #print("iteration: ", i + 1, "   cost: ", cost, "r2: ", r2)
 
-    return w, b
+    return J_history, w, b
 
 
 
@@ -93,8 +101,39 @@ def compute_gradient_descent(X, Y, w_in, b_in, alpha, num_iterations):
 initial_w = np.zeros(len(x[0]))
 initial_b = 0
 alpha = 0.1
-num_itr = 50
+num_itr = 60
 
-print("start processing")
-w, b = compute_gradient_descent(x_train, y_train, initial_w, initial_b, alpha, num_itr)
-print("w: ", w, "   b: ", b)
+start_time = time.time()
+J_his, w, b = compute_gradient_descent(x_train, y_train, initial_w, initial_b, alpha, num_itr)
+elapsed_time = time.time() - start_time
+#print("w: ", w, "   b: ", b)
+
+
+result = "PRICE = "
+result += str(b)
+for i in range(len(w)):
+    result += (str(w[i]) + " * " + df_x.columns[i])
+    if i != len(w) - 1:
+        result += " + "
+
+result += ("Traning Time: ",elapsed_time,"s\n")
+
+y_predict = []
+for x in x_test:
+    y_predict.append(compute_f(x, w, b))
+
+mse = mean_squared_error(y_test, y_predict)
+rmse = math.sqrt(mse)
+mae = mean_absolute_error(y_test, y_predict)
+r2 = r2_score(y_test, y_predict)
+
+result += ("Logs:")
+result += ("MSE: ", mse)
+result += ("RMSE: ",rmse)
+result += ("MAE: ",mae)
+result += ("R2: ",r2)
+
+
+with open('17-UIAI4021-PR1-Q2.txt', 'w', encoding='utf-8') as file:
+    file.write(result)
+
